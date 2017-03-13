46019b2b6e87c7547483037ab8dcbd6f1a48514b
Mateus Galvão <mateus@uololou.com.br>
2017-03-13 09:55:57 +0100

    Reading from yml

--- crawler.py
+++ crawler.py
@@ -6,6 +6,8 @@ import datetime as dt
 import numpy as np
 import pandas as pd
 import skyscanner.skyscanner as skc
+import yaml
+import sys
 
 from GeoBases import *
 from collections import defaultdict
@@ -14,47 +16,27 @@ from operator import itemgetter
 pd.set_option('display.width', 500)
 pd.set_option('display.max_rows', 1000)
 
+
+# helpers
+
 def _add_days(date, days):
     return date + dt.timedelta(days=days)
 
-
-# setup
-
-SETTINGS = {
-    'query'    : {
-        'market'   : 'NL',
-        'currency' : 'EUR',
-        'locale'   : 'pt-BR',
-    }
-}
-
-CRITERIA = {
-    'place_from': [ 'Amsterdam' ],
-    'place_to'  : [ 'Warsaw', 'Lisbon', 'Porto', 'Innsbruck', 'Inverness', 'Bergen', 'Rome', 'Florence', 'Reyjavík', 'Lugano', 'Dubrovnik'  ],
-    'direct'    : True,
-    'flying_day': [ 'Thursday', 'Friday', 'Saturday' ],
-    'length'    : [ 3, 4 ],
-    'date_begin': _add_days( dt.datetime.today(), 14),
-    'date_end'  : _add_days( dt.datetime.today(), 120),
-}
-
-
 geo = GeoBase(data='ori_por', verbose=False)
 def _get_code(name):
-    _, _id = geo.fuzzyFind(name, 'city_name_utf')[0]
-    return geo.get( _id )[ 'city_code' ]
+    scr, id = geo.fuzzyFind(name, 'city_name_utf')[0]
+    place   = geo.get( id )
+    return place[ 'city_code' ]
 
 def _api_query(**query):
-    api = skc.FlightsCache(SETTINGS['api_key'])
+    api = skc.FlightsCache(SETTINGS['api']['api_key'])
     kwargs = dict( SETTINGS['query'].items() + query.items() )
-    # return api.get_cheapest_price_by_route( **kwargs ).parsed
-    # return api.get_cheapest_price_by_date(**kwargs).parsed
     return api.get_cheapest_quotes(**kwargs).parsed
 
 def _get_dates():
     dates   = []
-    current = CRITERIA['date_begin']
-    ending  = CRITERIA['date_end']
+    current = _add_days(dt.datetime.today(), CRITERIA['days_begin'])
+    ending  = _add_days(dt.datetime.today(), CRITERIA['days_end'])
     while current < ending:
         if calendar.day_name[ current.weekday() ] in CRITERIA['flying_day']:
             for l in CRITERIA['length']:
@@ -77,11 +59,17 @@ def _get_best_quote(d, p, only_direct=False):
     for q in results['Quotes']:
         if only_direct and not q['Direct']: continue
         if best['MinPrice'] is None or q['MinPrice'] < best['MinPrice']: best = q
-    # print "{} > {}: {}".format(p[0], p[1], best['MinPrice'])
+    if SETTINGS['api']['verbose']: print "{} > {}: {}".format(p[0], p[1], best['MinPrice'])
     return best
 
 
-dates  = _get_dates()[:10]
+# execution
+
+with open('config.yml') as f:
+    SETTINGS = yaml.safe_load(f)
+    CRITERIA = SETTINGS[ sys.argv[1] ]
+
+dates  = _get_dates()
 places = _get_places()
 print "%d destinations x %d days" % (len(places), len(dates)) 
 
@@ -93,9 +81,8 @@ for i,d in enumerate(dates):
     quotes = [ _get_best_quote(d, p, only_direct=CRITERIA['direct']) for p in places ]
     prices.ix[i] = [ d[0], d[1] ] + [ q['MinPrice'] for q in quotes ]
 
-    if (i+1) % 10 == 0:
-        print "%d combinations" % ((i+1) * len(places))
-        time.sleep(5)
+    if (i+1) % 10 == 0: print "%d combinations" % ((i+1) * len(places))
+    time.sleep( float(SETTINGS['api']['limit_per_minute']) / len(places))
 
 print prices
 
